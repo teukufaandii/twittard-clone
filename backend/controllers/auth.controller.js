c679db391a1ac2dff9145b1c2cf7ba97be8c105a
Teuku Fandi Ahmad <tfandiahmad32@gmail.com>
2024-08-14 22:59:04 +0700

    setup setup manja

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,11 @@
+export const signup = async (req, res) => {
+  res.json({ msg: "signup" });
+};
+
+export const login = async (req, res) => {
+  res.json({ msg: "login" });
+};
+
+export const logout = async (req, res) => {
+  res.json({ msg: "logout" });
+};
---
+++ backend/server.js
@@ -0,0 +1,19 @@
+import express from "express";
+import dotenv from "dotenv";
+
+import authRoutes from "./routes/auth.routes.js";
+import connectMongoDB from "./db/connectMongoDB.js";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use("/api/auth", authRoutes);
+
+console.log(process.env.MONGO_URI);
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+  connectMongoDB();
+});
