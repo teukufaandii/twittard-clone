177e915de02815b50fc9d98d543032615419c282
Teuku Fandi Ahmad <tfandiahmad32@gmail.com>
2024-08-17 18:17:07 +0700

    adding user controller, follow/unfollow

---
+++ backend/controllers/user.controller.js
@@ -0,0 +1,59 @@
+import User from "../models/user.model.js";
+
+export const getUserProfile = async (req, res) => {
+  const { username } = req.params;
+
+  try {
+    const user = await User.findOne({ username }).select("-password");
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+    console.log("Error in getUserProfile controller", error.message);
+  }
+};
+
+export const followUnfollowUser = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const userToModify = await User.findById(id);
+    const currentUser = await User.findById(req.user._id);
+
+    if (id === req.user._id.toString()) {
+      return res
+        .status(400)
+        .json({ error: "You can't follow or unfollow yourself" });
+    }
+
+    if (!userToModify || !currentUser) {
+      return res.status(404).json({ error: "User not found" });
+    }
+
+    const isFollowing = currentUser.following.includes(id);
+    if (isFollowing) {
+      // Unfollow user
+      await User.findByIdAndUpdate(id, {
+        $pull: { followers: req.user._id },
+      });
+      await User.findByIdAndUpdate(req.user._id, {
+        $pull: { following: id },
+      });
+      res.status(200).json({ message: "Unfollowed successfully" });
+    } else {
+      // Follow user
+      await User.findByIdAndUpdate(id, {
+        $push: { followers: req.user._id },
+      });
+      await User.findByIdAndUpdate(req.user._id, {
+        $push: { following: id },
+      });
+      // send notification to the user
+      res.status(200).json({ message: "Followed successfully" });
+    }
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+    console.log("Error in followUnfollowUser controller", error.message);
+  }
+};
--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.routes.js";
+import userRoutes from "./routes/user.routes.js";
+
 import connectMongoDB from "./db/connectMongoDB.js";
-import cookieParser from "cookie-parser";
 
 dotenv.config();
 
@@ -16,6 +18,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
 
 console.log(process.env.MONGO_URI);
 
